b24a1cbdcb680311784895088aeea4897cc15e13
AlanOctavius <obrienad@googlemail.com>
2015-08-22 12:11:38 +0100

    Initial Insert
    Basic assets and test movment

---
+++ BFBDO/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public float speed;
+
+	Rigidbody2D rigidbody2d;
+
+
+	void Start(){
+
+		rigidbody2d = GetComponent<Rigidbody2D> ();
+
+	}
+
+
+	// Fixed Update is called once per frame
+	void FixedUpdate () {
+
+			float horizontalMovement = Input.GetAxis ("Horizontal");
+			float verticalMovement = Input.GetAxis ("Vertical");
+
+			rigidbody2d.angularVelocity = 0;
+
+			//prevent diagonal speed up
+			Vector2 playerForce = new Vector3 (horizontalMovement, verticalMovement, 0);
+			playerForce.Normalize ();
+
+			rigidbody2d.velocity =  (playerForce * speed);
+
+	}
+
+
+
+
+};
