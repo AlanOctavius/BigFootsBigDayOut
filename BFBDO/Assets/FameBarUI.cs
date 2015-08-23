4e2996cad91a03e345d77d4cee4df86eeb764272
Nick S Proud <nicksproud@gmail.com>
2015-08-23 16:39:06 +0100

    Did a Fame Bar!

---
+++ BFBDO/Assets/FameBarUI.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class FameBarUI : MonoBehaviour {
+
+	public HealthyFameFame FameController;
+
+	Image image;
+
+	// Use this for initialization
+	void Start () {
+		image = gameObject.GetComponent<Image> ();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		image.fillAmount = FameController.GetFame();
+	}
+}
