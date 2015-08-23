b4a957b8c56105804c7712e8d5b462e807ccafae
AlanOctavius <obrienad@googlemail.com>
2015-08-23 22:07:44 +0100

    Final Jam Commit
    Too ambitious

--- BFBDO/Assets/Scripts/PhotoScript.cs
+++ BFBDO/Assets/Scripts/PhotoScript.cs
@@ -12,7 +12,9 @@ public class PhotoScript : MonoBehaviour {
 
 	private float targetAngle;
 	public enum SearchStatus{NotSearching, Searching, ChaseingPlayer};
+	public enum Direction{Up,Down,Left,Right};
 
+	public Direction currentDirection = Direction.Up;
 	public SearchStatus searchStatus = SearchStatus.NotSearching;
 	public float averageSearchTimeRange = 5;
 	public float averageSearchTimeMin = 5;
@@ -25,11 +27,18 @@ public class PhotoScript : MonoBehaviour {
 	public float currentSearchTimeMax;
 	public float oscilationsPerSecond = 1;
 
+	public float debugAngel;
+	public float debugAngel2;
 
+	public Animator anim;
+
+	private bool facingLeft = false;
 	private List<Transform> waypoints;
 	private int currentWaypointIndex;
 
 	private Quaternion initialRotation;
+	private Transform oldTransform;
+	private Vector3 oldPosition;
 
 	// Use this for initialization
 
@@ -40,6 +49,9 @@ public class PhotoScript : MonoBehaviour {
 		focusScript = photoArea.transform.FindChild ("focus").gameObject.GetComponent<FocusScript>();
 		Transform waypointHolder = GameObject.FindGameObjectWithTag ("waypoint").transform;
 
+		anim = GetComponent<Animator>();
+
+		oldTransform = transform;
 		waypoints = new List<Transform> ();
 		foreach (Transform waypoint in waypointHolder){
 			waypoints.Add(waypoint);
@@ -97,6 +109,8 @@ public class PhotoScript : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
+		oldTransform = transform;
+		oldPosition = transform.position;
 		if (searchStatus == SearchStatus.NotSearching) {
 			NotSearchingUpdate();
 		} else if (searchStatus == SearchStatus.Searching) {
@@ -104,6 +118,47 @@ public class PhotoScript : MonoBehaviour {
 		} else if (searchStatus == SearchStatus.ChaseingPlayer){
 			ChaseUpdate();
 		}
+
+		///TODO:
+		/// Pick direction and set trigger
+		/// 
+		float distance = Vector2.Distance ((Vector2)transform.position, (Vector2)oldPosition);
+		anim.SetFloat ("Speed", distance);
+		Vector3 vectorToTarget = oldPosition - transform.position ;
+		float angle =  Mathf.Atan2 (vectorToTarget.y, vectorToTarget.x) * Mathf.Rad2Deg + 90;
+		angle = nfmod (angle, 360f);
+		debugAngel = distance;
+		debugAngel2 = angle;
+		if (angle > 315 || angle <= 45 && currentDirection != Direction.Up) {
+			//up
+			currentDirection = Direction.Up;
+			anim.SetTrigger("GoingUp");
+		} else if (angle > 135 && angle <= 225 && currentDirection != Direction.Down) {
+			//down
+			currentDirection = Direction.Down;
+			anim.SetTrigger("GoingDown");
+		} else {
+			//sideways
+			if (angle > 45 && angle <= 135 && currentDirection != Direction.Right){
+				// right
+				currentDirection = Direction.Right;
+				anim.SetTrigger("GoingSideways");
+				if(facingLeft){
+					Flip();
+				}
+			}else if (angle > 225 && angle <= 315 && currentDirection != Direction.Left){
+				//left
+				currentDirection = Direction.Left;
+				anim.SetTrigger("GoingSideways");
+				if(!facingLeft){
+					Flip();
+				}
+			}
+
+		}
+
+
+
 	}
 
 	void NotSearchingUpdate(){
@@ -117,12 +172,11 @@ public class PhotoScript : MonoBehaviour {
 			currentSearchTimeMax = averageSearchTimeRange * random + averageSearchTimeMin;
 
 			//get waypoint target
-			Vector3 targetPos = waypoints[currentWaypointIndex].FindChild("Target").position;
+			Transform target = waypoints[currentWaypointIndex].FindChild("Target");
 
 
 			//rotate camera area to waypoint target
-			Vector3 vectorToTarget = transform.position - targetPos;
-			targetAngle = Mathf.Atan2 (vectorToTarget.y, vectorToTarget.x) * Mathf.Rad2Deg ;
+			targetAngle = getAngle(transform, target);
 			photoArea.transform.rotation = Quaternion.AngleAxis (targetAngle, Vector3.forward);
 
 			changeStatusTo(SearchStatus.Searching);
@@ -168,8 +222,7 @@ public class PhotoScript : MonoBehaviour {
 			}
 		} else {
 
-			Vector3 vectorToTarget = transform.position - photoTarget.position ;
-			float angle = Mathf.Atan2 (vectorToTarget.y, vectorToTarget.x) * Mathf.Rad2Deg ;
+			float angle = getAngle(transform, photoTarget);
 
 			//create the rotation we need to be in to look at the target
 			//Quaternion lookRotation = Quaternion.LookRotation(direction, Vector3.down);
@@ -182,7 +235,27 @@ public class PhotoScript : MonoBehaviour {
 			//focusScript.gameObject.transform.RotateAround(transform.position,
 			//                                              Vector3.forward,
 			//                                              angle);
+			Vector3 vectorToTarget = transform.position - photoTarget.position ;
 			focusScript.SetTarget (((Vector2)vectorToTarget).magnitude);
 		}
+	}
+
+	private float getAngle(Transform start, Transform end){
+		Vector3 vectorToTarget = start.position - end.position ;
+		return  Mathf.Atan2 (vectorToTarget.y, vectorToTarget.x) * Mathf.Rad2Deg ;
+	}
+
+	private void Flip()
+	{
+		// Switch the way the player is labelled as facing
+		facingLeft = !facingLeft;
+		
+		// Multiply the player's x local scale by -1
+		Vector3 theScale = transform.localScale;
+		theScale.x *= -1;
+		transform.localScale = theScale;
+	}
+	float nfmod(float a,float b){
+		return a - b * Mathf.Floor(a / b);
 	}
 }
